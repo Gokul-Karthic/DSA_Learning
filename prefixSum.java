3635a8b5b014cf2587f3d847fd57a3876b634a2e
Gokul-Karthic <gokul.23mim10098@vitbhopal.ac.in>
2025-08-06 23:00:18 +0530

    prefixSum_java

---
+++ prefixSum.java
@@ -0,0 +1,13 @@
+class prefixSum{
+    public static void main(String[]args){
+        //prefixsum
+        int [] arr = {2,1,3,4,5};
+//        
+        for(int i = 1 ;i<arr.length;i++){
+            arr[i] = arr[i]+arr[i-1];
+        }
+        for(int i =0 ; i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
