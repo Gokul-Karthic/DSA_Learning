0197d805d08b03b74b0245f0726b85d878389e44
Gokul-Karthic <gokul.23mim10098@vitbhopal.ac.in>
2025-08-07 11:11:47 +0530

    equalSumsubarray

---
+++ equalSumsubarray.java
@@ -0,0 +1,24 @@
+public class equalSumsubarray {
+      public static void main(String[] args) {
+          int[]arr= {1,2,3,4,5};
+          int n = arr.length;
+          int []prefix = new int[n];
+          int []suffix = new int [n];
+          prefix[0]=arr[0];
+          suffix[n-1]=arr[n-1];
+          for(int i = 1 ;i<n;i++){
+            prefix[i]=prefix[i-1]+arr[i];
+          }
+          for(int i=n-2;i>=0;i--){
+            suffix[i]=suffix[i+1]+arr[i];
+          }
+          int j= arr.length-1;
+          for(int i=0;i<n-1;i++){
+            if(prefix[i]==suffix[j--]){
+                  System.out.println("yes");
+                  return ;
+            }
+          }
+          System.out.println("no");
+      }
+}
---
+++ sumbetwenindexprefix.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class sumbetwenindexprefix {
+    public static void main(String [] args){
+      Scanner sc = new Scanner(System.in);
+      System.out.println("enter size - ");
+      int n = sc.nextInt();
+      //Array input
+      int [] arr = new int[n+1];
+      for (int i =1; i<=n;i++){
+            arr[i] = sc.nextInt();
+      }
+      //Prefix Array
+      for(int i =1;i<=n;i++){
+            arr[i] = arr[i]+arr[i-1];
+      }
+      System.out.println("enter start and end- ");
+      int start = sc.nextInt();
+      int end = sc.nextInt();
+      int sum = arr[end]-arr[start-1];
+      System.out.println("sum- "+sum);
+
+
+
+     
+
+    }
+}
